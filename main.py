e510a05bcd74ffabd869c2b33ce83ce19a91cb78
HunKang98 <99076620+HunKang98@users.noreply.github.com>
2025-08-12 23:07:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from RiForest import *
 from utils import *
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import StandardScaler
-import time
 
 path_data=r"./dataset/thyroid.mat"
     
@@ -17,12 +16,7 @@ scaler = StandardScaler()
 data_X = scaler.fit_transform(data_X)
 
 iso = RiForest(sample_size=256, n_trees=100, bins=10, n_iter=5, alpha=0.8)
-start = time.time()
 iso.fit(data_X)
 anomaly_score = iso.anomaly_score(data_X)
-end = time.time()
 
 print(roc_auc_score(data_label, anomaly_score))
-print(end-start)
-
-    
